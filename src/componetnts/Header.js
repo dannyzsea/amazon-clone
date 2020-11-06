469879e5e26e4bf19fe17c8c80373aaa5b127109
John Doe <javatechisland@gmail.com>
2020-11-05 19:23:50 -0800

    cart , nav, icons

--- src/componetnts/Header.js
+++ src/componetnts/Header.js
@@ -1,11 +1,17 @@
 import React from 'react'
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
+import ViewHeadlineOutlinedIcon from '@material-ui/icons/ViewHeadlineOutlined';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import ExpandMoreOutlinedIcon from '@material-ui/icons/ExpandMoreOutlined';
 
 function Header() {
     return (
         //BEM convention for className
         <div className="header">
+        
+<ViewHeadlineOutlinedIcon className="header__iconm"/>
+
           <img className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
               alt="amazon"
@@ -25,15 +31,19 @@ function Header() {
         <div className="header__navInner">
             <span className="header__navInnerLineONe"> Hello</span>
             <span className="header__navInnerLineTwo">Accounts & Lists </span>
+             <ExpandMoreOutlinedIcon className="header__navInnerLineTwoIcon"/>
         </div>
         <div className="header__navInner">
             <span className="header__navInnerLineONe"> Returns</span>
             <span className="header__navInnerLineTwo"> & Orders </span>
+            <ExpandMoreOutlinedIcon className="header__navInnerLineTwoIcon"/>
         </div>
         <div className="header__navInner">
             <span className="header__navInnerLineONe"> Try Prime</span>
         </div>
-        
+        <div className="header__navInner">
+            <ShoppingCartOutlinedIcon className="header__navInnerCart"/>
+        </div>
         </div>
 
         </div>
